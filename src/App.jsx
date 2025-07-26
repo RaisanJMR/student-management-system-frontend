0fc01c96d2944d8bb403663d07e0f2e694ea1abc
RaisanJMR <raisanjmr@protonmail.com>
2025-07-27 00:55:48 +0530

    optimized components

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,52 @@
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
 import DashboardLayout from './Layout/DashboardLayout'
-import StudentPage from './pages/StudentPage'
-import StaffPage from './pages/StaffPage'
 import ProtectedRoutes from './ProtectedRoute'
 import RequireAuth from './RequireAuth'
-import Unauthorized from './pages/Unauthorized'
-import NotFoundPage from './pages/NotFound'
 
+// Lazy loaded pages
+const LoginPage = React.lazy(() => import('./pages/LoginPage'))
+const RegisterPage = React.lazy(() => import('./pages/RegisterPage'))
+const StudentPage = React.lazy(() => import('./pages/StudentPage'))
+const StaffPage = React.lazy(() => import('./pages/StaffPage'))
+const Unauthorized = React.lazy(() => import('./pages/Unauthorized'))
+const NotFoundPage = React.lazy(() => import('./pages/NotFound'))
 
 function App() {
   return (
     <div className='app'>
       <Router>
-        <Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
 
-          <Route path="/" element={<Navigate to="/login" replace />} />
-
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route element={<ProtectedRoutes />}>
-            <Route
-              path="/dashboard"
-              element={
-                <DashboardLayout />
-              }
-            >
-              <Route index element={
-                <RequireAuth allowedRoles={['super_admin', 'staff']}>
-                  <StudentPage />
-                </RequireAuth>
-              } />
-              <Route path="staff" element={
-                <RequireAuth allowedRoles={['super_admin']}>
-                  <StaffPage />
-                </RequireAuth>
-              } />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/dashboard" element={<DashboardLayout />}>
+                <Route
+                  index
+                  element={
+                    <RequireAuth allowedRoles={['super_admin', 'staff']}>
+                      <StudentPage />
+                    </RequireAuth>
+                  }
+                />
+                <Route
+                  path="staff"
+                  element={
+                    <RequireAuth allowedRoles={['super_admin']}>
+                      <StaffPage />
+                    </RequireAuth>
+                  }
+                />
+              </Route>
             </Route>
 
-          </Route>
-          <Route path="/unauthorized" element={<Unauthorized />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+            <Route path="/unauthorized" element={<Unauthorized />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   )
