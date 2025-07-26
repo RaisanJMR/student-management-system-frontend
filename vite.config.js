5dcbd5f97473c26405dd50650c226ef20b197976
RaisanJMR <raisanjmr@protonmail.com>
2025-07-27 01:17:27 +0530

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/student-management-system-frontend/',
+  base: '/student-management-system-frontend',
   build: {
     rollupOptions: {
       output: {
