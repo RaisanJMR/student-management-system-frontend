5e30a72f68c54a55b4182da4ed2357066bb4d90d
RaisanJMR <raisanjmr@protonmail.com>
2025-07-27 01:05:02 +0530

    added base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/student-management-system-frontend/',
   build: {
     rollupOptions: {
       output: {
@@ -14,3 +15,4 @@ export default defineConfig({
     }
   }
 })
+
