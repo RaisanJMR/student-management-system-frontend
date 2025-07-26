dcde2881f7ad1d2d7db50eb010aae138defe8951
RaisanJMR <raisanjmr@protonmail.com>
2025-07-27 01:37:55 +0530

    deploy vercel v1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/student-management-system-frontend',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
